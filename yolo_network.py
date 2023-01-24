b29d2a7b0b4050703c5c0a199c7aa9d1ce4c7c71
cagey-squirrel <cvetkovicaleksandar1302@gmail.com>
2023-01-24 03:28:23 +0100

    Yolo function for output, preparing for loss

--- yolo_network.py
+++ yolo_network.py
@@ -3,14 +3,16 @@ from PIL import Image
 from matplotlib import pyplot as plt
 import numpy as np
 import warnings
-from draw_rect import show_images_with_boxes
+from draw_rect import show_images_with_boxes, load_annotations, get_labels_from_annotations, load_images, display_images_with_bounding_boxes
+from loss_functions import YOLOLoss
+import os
 
 warnings.filterwarnings("ignore")
 
 class TinyYOLOv2(torch.nn.Module):
     def __init__(
         self,
-        num_classes=4,
+        num_classes=6,
         anchors=(
             (1.08, 1.19),
             #(3.42, 4.41),
@@ -31,21 +33,21 @@ class TinyYOLOv2(torch.nn.Module):
         self.slowpool = torch.nn.MaxPool2d(2, 1)
         self.pad = torch.nn.ReflectionPad2d((0, 1, 0, 1))
         self.norm1 = torch.nn.BatchNorm2d(16, momentum=0.1)
-        self.conv1 = torch.nn.Conv2d(3, 16, 5, 1, 1, bias=False)
+        self.conv1 = torch.nn.Conv2d(4, 16, 3, 1, 1, bias=False)
         self.norm2 = torch.nn.BatchNorm2d(32, momentum=0.1)
-        self.conv2 = torch.nn.Conv2d(16, 32, 5, 1, 1, bias=False)
+        self.conv2 = torch.nn.Conv2d(16, 32, 3, 1, 1, bias=False)
         self.norm3 = torch.nn.BatchNorm2d(64, momentum=0.1)
-        self.conv3 = torch.nn.Conv2d(32, 64, 5, 1, 1, bias=False)
+        self.conv3 = torch.nn.Conv2d(32, 64, 3, 1, 1, bias=False)
         self.norm4 = torch.nn.BatchNorm2d(128, momentum=0.1)
-        self.conv4 = torch.nn.Conv2d(64, 128, 5, 1, 1, bias=False)
+        self.conv4 = torch.nn.Conv2d(64, 128, 3, 1, 1, bias=False)
         self.norm5 = torch.nn.BatchNorm2d(256, momentum=0.1)
-        self.conv5 = torch.nn.Conv2d(128, 256, 5, 1, 1, bias=False)
+        self.conv5 = torch.nn.Conv2d(128, 256, 3, 1, 1, bias=False)
         self.norm6 = torch.nn.BatchNorm2d(512, momentum=0.1)
-        self.conv6 = torch.nn.Conv2d(256, 512, 5, 1, 1, bias=False)
+        self.conv6 = torch.nn.Conv2d(256, 512, 3, 1, 1, bias=False)
         self.norm7 = torch.nn.BatchNorm2d(1024, momentum=0.1)
-        self.conv7 = torch.nn.Conv2d(512, 1024, 5, 1, 1, bias=False)
+        self.conv7 = torch.nn.Conv2d(512, 1024, 3, 1, 1, bias=False)
         self.norm8 = torch.nn.BatchNorm2d(1024, momentum=0.1)
-        self.conv8 = torch.nn.Conv2d(1024, 1024, 5, 10, 1, bias=False)
+        self.conv8 = torch.nn.Conv2d(1024, 1024, 3, 1, 1, bias=False)
         self.conv9 = torch.nn.Conv2d(1024, len(anchors) * (5 + num_classes), 1, 1, 0)
 
     def forward(self, x, yolo=True):
@@ -58,9 +60,60 @@ class TinyYOLOv2(torch.nn.Module):
         x = self.relu(self.norm7(self.conv7(x)))
         x = self.relu(self.norm8(self.conv8(x)))
         x = self.conv9(x)
-        if yolo:
-            x = self.yolo(x)
+        #if yolo:
+        #    x = self.yolo(x)
+        # shape of x is now (Batch_size, [5+num_classes], height_in_cells, width_in_cells)
+        # 1 11 12 18
+        #print(x.shape)
+        x = self.my_yolo(x)
+        
         return x
+
+    def my_yolo(self, x, for_output=True):
+        x = torch.permute(x, (0, 3, 2, 1)) # (Batch, width, height, [5+num_classes])
+
+        num_cells_width = 18
+        num_cells_height = 12
+        # [5 + num_classes = center_x, center_y, width, height, conf + ONE_HOT_CLASS_VECTOR]
+        x[..., 0] = x[..., 0].sigmoid()
+        x[..., 1] = x[..., 1].sigmoid()
+        x[..., 2] = x[..., 2].exp()
+        x[..., 3] = x[..., 3].exp()
+        x[..., 4] = x[..., 4].sigmoid()
+        x[..., 5:] = x[..., 5:].softmax(-1)
+
+        #print(f'basic x shape = {x.shape}')
+        #print(f'x shape = {x[..., 0].shape}')
+        #print(f'torch shape = {torch.arange(cells_width).shape}')
+        #exit(-1)
+        if for_output:
+            x[..., 0] += torch.arange(num_cells_width)[None, :, None]
+            x[..., 1] += torch.arange(num_cells_height)[None, None, :]
+
+        #print(torch.arange(num_cells_width)[None, :, None])
+        #print(torch.arange(num_cells_height)[None, None, :])
+        #print(x[...,0:2])
+        return x
+    
+    def loss(self, x, labels):
+
+        # Loss = loss_class + loss_conf + loss_coord
+        # Od svih predikcija u jednoj celiji treba izabrati najsigurniju -> ovo je odradjeno jer ima samo jedna predikcija po celiji
+        # Od svih anchora u jednoj celiji treba izabrati onaj koji se najbolje uklapa u labelu -> ovo je odradjeno jer ima samo jedan anchor po celiji
+
+        # Dakle treba odrediti u kojoj celiji je objekat a u kojoj nije, ovo moze i kroz labele
+        # Treba primeniti odgovarajuci loss na sve delove
+
+        # Recimo da labels stize u formatu (B, A, W, H, (Cx, Cy, Wx, Wy, ONE_HOT_CLASS_ENCODINGS))
+        # Shape is (B, A, W, H, 5+C)
+
+        # Sta uzimamo za loss: Uzimamo rastojanje od gornjeg levog ugla do sredine objekta u celiji 
+        # U labeli je meni dato za sreidnu slike vrednost centra npr 0.55, rastojanje od gornjeg 
+
+        object_present = labels[..., 5] # getting the certainty from labels: 1 for object present, 0 for object absent 
+        object_not_present = torch.logical_not(object_present)
+
+
     
     def yolo(self, x):
 
@@ -82,6 +135,8 @@ class TinyYOLOv2(torch.nn.Module):
             torch.arange(nH, dtype=x.dtype, device=x.device),
             torch.arange(nW, dtype=x.dtype, device=x.device),
         )
+
+        #print(range_x)
   
         # compute boxes.
         x = torch.cat([
@@ -95,20 +150,81 @@ class TinyYOLOv2(torch.nn.Module):
 
         # Za svaku celiju imamo po predikciju za centar x-a 
         # Ta predikcija pomocu relu ide izmedju 0 i 1 i govori koliko je pomerena od centra izrazeno u procentima od sirine 
-        print(x[:, :, :, :, 0, None].squeeze())
-        print(range_x[None,None,:,:,None].squeeze())
+        #print(x[:, :, :, :, 0, None].squeeze())
+        #print(range_x[None,None,:,:,None].squeeze())
 
         return x # (batch_size, # anchors, height, width, 5+num_classes)
+    
 
 def main():
+
+    num_cells_height = 12 
+    num_cells_width = 18
+    cell_height = 32
+    cell_width = 32
+    anchor_height = 250
+    anchor_width = 300
+    
+
+
     torch.manual_seed(1302)
     tyv2 = TinyYOLOv2()
-    rook = torch.Tensor(np.array(Image.open("rook.jpg")))
+    rook = torch.Tensor(np.array(Image.open("all_same_size_imgs/jeremija.png")))
     rook = torch.permute(rook, (2, 0, 1))
     rook = rook[None, :, :, :]
+    print(f'image shape = {rook.shape}')
     output = tyv2.forward(rook)
-    print(f'output shape is {output.shape}')
-    show_images_with_boxes(rook, output)
+    #display_images_with_bounding_boxes(rook[0], output[0, 0, :, :], cell_width, cell_height, anchor_width, anchor_height)
+    #exit(-1)
+    #print(f'output shape is {output.shape}')
+    #exit(-1)
+    #show_images_with_boxes(rook, output)
     #print(f'example of output is: {output[0, 0, 0, 0, :]}')
+
+    images_dir = 'all_same_size_imgs'
+    annotations_path = 'annotations/annotations2.csv'
+    
+    
+    
+    annotations = load_annotations(annotations_path)
+    labels = get_labels_from_annotations(annotations, 389, 588)
+
+    images = load_images(images_dir)
+#
+    for image_key in images:
+        image = images[image_key]
+        label = labels[image_key]
+        
+        label = label.view(label.shape[0] * label.shape[1] * label.shape[2], label.shape[3])
+        print(f'label_shape = {label.shape}')
+        display_images_with_bounding_boxes(image, label, cell_width, cell_height, anchor_width, anchor_height, name=image_key)
+
+
+
+    #print(labels)
+    exit(-1)
+
+    jeremija_label = labels['jeremija']
+    jeremija_label = jeremija_label.squeeze()
+    jeremija_label = jeremija_label.tolist()
+    jeremija_labels = [jeremija_label for _ in range(12*18-1)]
+    jeremija_labels = np.array(jeremija_labels)
+    #print(jeremija_labels)
+    jeremija_labels = torch.Tensor(jeremija_labels)
+    zeross = torch.zeros((1,12))
+    zeross[..., -1] = -1
+    jeremija_labels = torch.cat([jeremija_labels, zeross], 0)
+    jeremija_labels = jeremija_labels[None, ...]
+
+
+    #raw_output = tyv2.forward(rook, yolo=False)
+    #loss = YOLOLoss()
+    #loss_val = loss.forward(raw_output, jeremija_labels)
+    #print(loss_val)
+
+    #output = tyv2.forward(rook, yolo=True)
+    #show_images_with_boxes(rook, output)
+
+    
 
 main()
