3a86bb12a763ce13d98a2b169d479834145e2652
cagey-squirrel <cvetkovicaleksandar1302@gmail.com>
2023-02-11 01:39:10 +0100

    Saving predictions and labels drawn on images

--- draw_rect.py
+++ draw_rect.py
@@ -102,4 +102,73 @@ def display_images_with_bounding_boxes(image, bounding_boxes, classes, cell_widt
         axis.add_patch(rect)
 
     plt.show()
+
+
+def add_bounding_boxes_to_axis(bounding_boxes, axis, classes, height_and_width_info, color):
+
+    image_height, image_width, cell_width, cell_height, anchor_width, anchor_height = height_and_width_info
+    bounding_boxes = bounding_boxes.reshape(-1, bounding_boxes.shape[-1])
+    
+    for bounding_box in bounding_boxes:
+
+        if not bounding_box.any():
+            continue
+
+        # Bounding box contains: (box_center_x, box_center_y, box_width, box_height, confidence, CLASS_ONE_HOT_ENCODING)
+        # Box_center_x, and Box_center_y are in units of cells so we need to multiply them to display them on image
+        # box_width and box_height are in units of anchor sizes so they too need to be multiplied in order to be displayed
+
+        box_center_x = bounding_box[0] * cell_width
+        box_center_y = bounding_box[1] * cell_height 
+
+        box_width  = bounding_box[2] * anchor_width 
+        box_height = bounding_box[3] * anchor_height
+
+        # Class Rectangle needs top left corner as input
+        top_left_corner_x = box_center_x - box_width  / 2
+        top_left_corner_y = box_center_y - box_height / 2
+
+        class_probabilities = bounding_box[5:]
+        class_max_index = class_probabilities.argmax()
+        object_class = classes[class_max_index]
+
+        # Making sure the box fits the image (doesnt go beyond)
+        top_left_corner_x = min(max(1, top_left_corner_x), image_width-1)
+        top_left_corner_y = min(max(1, top_left_corner_y), image_height-1)
+
+        rect = matplotlib.patches.Rectangle((top_left_corner_x, top_left_corner_y), box_width, box_height, fill=False, edgecolor=color) 
+        axis.text(x=top_left_corner_x + 10, y = top_left_corner_y + 20, s = object_class, color = 'red')
+        axis.add_patch(rect)
+
+
+def output_predictions(images, labels_list, predictions, images_names, classes, height_and_width_info, output_dir_path, epoch_num):
+    '''
+    Outputs prediction detections and true label boxes on images
+
+    Input:
+        - images:
+        - labels
+        - predictions
+        - images_names
+    '''
+
+    output_dir = os.path.join(output_dir_path, str(epoch_num))
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    images = images.detach().cpu().numpy()
+    labels_list = labels_list.detach().cpu().numpy()
+    predictions = predictions.detach().cpu().numpy()
+
+    for image, labels, prediction, image_name in zip(images, labels_list, predictions, images_names):
+        fig, axis = plt.subplots()
+
+        image = np.transpose(image, (1,2,0))
+        axis.imshow(image)
+
+        add_bounding_boxes_to_axis(labels, axis, classes, height_and_width_info, color='green')
+        add_bounding_boxes_to_axis(prediction, axis, classes, height_and_width_info, color='red')
+
+        plt.savefig(os.path.join(output_dir, image_name))
+
         
